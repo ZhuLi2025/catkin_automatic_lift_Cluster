6e19e30464dcc7faf6b103031d183d3e03f8775c
Karry11 <2453635519@qq.com>
2026-02-06 11:50:31 +0800

    路径规划so引入（未修改mainwindow）
    路径规划so引入（未修改mainwindow）

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ source devel/setup.bash
 # 设置海康库路径
 DIR="$( pwd )"
 export LD_LIBRARY_PATH=$DIR/src/zl_Crane_AutomaticLift_trunk/3rd_partys/HCNetSDK/lib:$DIR/src/zl_Crane_AutomaticLift_trunk/3rd_partys/HCNetSDK/lib/HCNetSDKCom:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$DIR/src/zl_Crane_AutomaticLift_trunk/third_party/path_generate:$LD_LIBRARY_PATH
 
 roslaunch zl_Crane_AutomaticLift_trunk zl_crane_automaticlift_trunk.launch
